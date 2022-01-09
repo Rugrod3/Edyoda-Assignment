7c37c0dbbd4472729ddb8d10e93a8d500dca4c1c
Rugved Rode <96370698+Rugrod3@users.noreply.github.com>
2022-01-09 15:06:35 +0530

    Add files via upload

---
+++ Edyoda assignment 4.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+add=lambda no:no+25
+add(12)
+
+
+# In[17]:
+
+
+sample= [1,2,3,4,5,6,7]
+list(map(lambda no:3*no,sample))
+
+
+# In[22]:
+
+
+no_list=[1,2,13,4,5,8,9]
+list(map(lambda number:number**2,no_list))
+
