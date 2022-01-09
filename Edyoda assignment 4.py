49935a7faa8ed2738b18453ad9540a3ec93c3cf5
Rugved Rode <96370698+Rugrod3@users.noreply.github.com>
2022-01-09 15:07:44 +0530

    Update Edyoda assignment 4.py

--- Edyoda assignment 4.py
+++ Edyoda assignment 4.py
@@ -1,23 +1,12 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[6]:
-
+Q.1
 
 add=lambda no:no+25
 add(12)
 
-
-# In[17]:
-
-
+Q.2
 sample= [1,2,3,4,5,6,7]
 list(map(lambda no:3*no,sample))
 
-
-# In[22]:
-
-
+Q.3
 no_list=[1,2,13,4,5,8,9]
 list(map(lambda number:number**2,no_list))
-
