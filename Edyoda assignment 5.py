ebdc602b06025b66ab6b34fdf3793a80ec475a63
Rugved Rode <96370698+Rugrod3@users.noreply.github.com>
2022-01-16 15:37:01 +0530

    Add files via upload

---
+++ Edyoda assignment 5.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+class number:
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+        
+    def power(self):
+        return self.x**self.y
+
+
+# In[7]:
+
+
+sample=number(2,3)
+
+
+# In[8]:
+
+
+sample.power()
+
+
+# In[ ]:
+
+
+
+
