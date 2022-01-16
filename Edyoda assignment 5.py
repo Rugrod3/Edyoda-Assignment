164145b6e6a7af0d568ae75bf44e9953e90bac85
Rugved Rode <96370698+Rugrod3@users.noreply.github.com>
2022-01-16 15:37:57 +0530

    Update Edyoda assignment 5.py

--- Edyoda assignment 5.py
+++ Edyoda assignment 5.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[6]:
-
 
 class number:
     def __init__(self,x,y):
@@ -12,21 +7,6 @@ class number:
     def power(self):
         return self.x**self.y
 
-
-# In[7]:
-
-
 sample=number(2,3)
 
-
-# In[8]:
-
-
 sample.power()
-
-
-# In[ ]:
-
-
-
-
